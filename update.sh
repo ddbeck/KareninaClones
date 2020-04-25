c6b5b7e7f2cd858428e3b1c91023683d32322af8
Daniel D. Beck <daniel.david.beck@gmail.com>
2020-04-25 21:11:14 +0100

    Drop outdated email

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ COMMAND='pipenv run kareninaclones --tweet --keyfile keys.json'
 OUTFILE="$KARENINA_CLONES_LOG_DIR/error_log"
 
 if ! $COMMAND &>"$OUTFILE"; then
-  mail -s "@KareninaClones failed at $(date)" "kareninaclones@ddbeck.com" <"$OUTFILE"
+  mail -s "@KareninaClones failed at $(date)" "$RECIPIENT" <"$OUTFILE"
 fi
 rm "$OUTFILE"
