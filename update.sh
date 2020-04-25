6f99ba4c1cdf099780ee22c45850ae336262eccb
Daniel D. Beck <daniel.david.beck@gmail.com>
2020-04-25 21:28:59 +0100

    move more to .env

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@
 # export LC_ALL=en_US.UTF-8
 # export LANG=en_US.UTF-8
 
-COMMAND='pipenv run kareninaclones --tweet --keyfile keys.json'
-OUTFILE="$KARENINA_CLONES_LOG_DIR/error_log"
+COMMAND="pipenv run kareninaclones --tweet --keyfile $KARENINA_CLONES_DIR/keys.json"
+OUTFILE="$KARENINA_CLONES_DIR/error_log"
 
 if ! $COMMAND &>"$OUTFILE"; then
   mail -s "@KareninaClones failed at $(date)" "$RECIPIENT" <"$OUTFILE"
